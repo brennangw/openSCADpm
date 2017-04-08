39f8a69bcbb4b44afc66a4c48cc456d041174034
skp2140 <skp2140@columbia.edu>
2017-04-08 01:31:07 -0400

    Added Static Pages controller

---
+++ pm_website/app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def OpenScadpm
+  end
+
+  def Documentation
+  end
+end
--- pm_website/config/routes.rb
+++ pm_website/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/OpenScadpm'
+
+  get 'static_pages/Documentation'
+
     root 'application#hello'
 end
