f8e00fb35f2a5a4699aded414c2a405f75b7be56
skp2140 <skp2140@columbia.edu>
2017-04-23 18:37:46 -0400

    Displaying package bug eliminated

--- pm_website/app/controllers/packages_controller.rb
+++ pm_website/app/controllers/packages_controller.rb
@@ -1,5 +1,5 @@
 class PackagesController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit]
+  before_action :logged_in_user, only: [:edit]
   def index
     @packages = Package.all
   end
