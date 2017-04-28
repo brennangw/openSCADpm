11c792e7e7fb312c5ab6be0348d145a124fbe86e
brennangw <b.wallace@columbia.edu>
2017-04-27 22:06:55 -0400

    untested and broken uninstall, commiting multiple approaches

--- ospm.sh
+++ ospm.sh
@@ -6,6 +6,9 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 
+slash="/"
+deps="dependencies"
+
 
 function library {
 	if [[ "$1" == "save" ]]; then
@@ -14,31 +17,214 @@ function library {
 		cat /usr/local/lib/ospmLibSettings
 	else
 		printf "${YELLOW}Module library is:\n${NC}"
-			output=$(cat /usr/local/lib/ospmLibSettings)
+		output=$(cat /usr/local/lib/ospmLibSettings)
 		printf "${YELLOW}$output\n${NC}"
-  fi
+	fi
 
 }
 
 function help {
-        printf "${YELLOW}Usage: ospm [-v] [-h] [library <> <>]\n${NC}"
-        printf "${YELLOW}-v                Show version\n${NC}"
-        printf "${YELLOW}-h                Show command line options\n${NC}"
-        printf "${YELLOW}library $2 $3         Save XXX in library\n${NC}"
+	printf "${YELLOW}Usage: ospm [-v] [-h] [library <> <>]\n${NC}"
+	printf "${YELLOW}-v                Show version\n${NC}"
+	printf "${YELLOW}-h                Show command line options\n${NC}"
+	printf "${YELLOW}library $2 $3         Save XXX in library\n${NC}"
+}
+
+#depmap
+declare -A nonTargetRequiredPackages
+declare -A targetRequiredPackages
+
+
+#uninstallHelper
+#recusively builds a map of required packages based on requirement graphs.
+#params
+# 1,2,3 are the auth, name, version of the package to check.
+# 4 is the libaray path.
+function nonTargetRequirementMapBuilder {
+	packageDir="$1-$2-$3"
+	#pacakge not added to map, so add it.
+	if [[ -z "${nonTargetRequiredPackages[$packageDir]}" ]]; then
+		libLoc=$4
+		nonTargetRequiredPackages[$packageDir]=True
+		while read -r dep; do
+			nonTargetRequirementMapBuilder $dep $4
+		done < $libLoc$slash$packageDir$slash$deps
+	fi
+	#package already added to map so doing nothing.
+}
+
+function targetRequirementMapBuilder {
+	packageDir="$1-$2-$3"
+	#pacakge not added to map, so add it.
+	if [[ -z "${targetRequiredPackages[$packageDir]}" ]]; then
+		libLoc=$4
+		targetRequiredPackages[$packageDir]=True
+		while read -r dep; do
+			targetRequirementMapBuilder $dep $4
+		done < $libLoc$slash$packageDir$slash$deps
+	fi
+	#package already added to map so doing nothing.
+}
+
+function uninstall {
+	libLoc=$(cat /usr/local/lib/ospmLibSettings)
+	packageDir="$1-$2-$3"
+	#some safety
+	if [ ! -z $libLoc ] && [ "$libLoc" != "/" ] && [ ! -z $1 ] && [ ! -z $2 ] && [! -z $3]; then
+		#for each package check if the target package is a requirement
+		#if so it is an easy stop (show all for ease) with little processing.
+		beingUsed=false
+		for d in $( ls -d $libLoc$slash*/ ) ; do #this wont work :(
+			if grep -Fxq "$1 $2 $3" $d$slash$deps
+			then
+				echo "$1-$2-$3 is being used in $d"
+				beingUsed=true
+			fi
+		done
+
+		if [ beingUsed = true ] ; then
+			echo "Becuase $1-$2-$3 is being used, it cannot be uninstalled."
+			return
+		fi
+
+		#from here we know that at least the target will be uninstalled.
+
+		#build a map of all packages that are diretcly or indirectly required by
+		#all packages besides the target package.
+
+		for d in $( ls -d $libLoc$slash*/ ) ; do
+			#check deps file to see if it is an ospm module.
+			if [[ -f $d$slash$deps ]]; then
+				#split on dashes
+				auth=$(echo $d | cut -d'-' -f1)
+				name=$(echo $d | cut -d'-' -f2)
+				version=$(echo $d | cut -d'-' -f3)
+				#call requirementMapBuilder
+				requirementMapBuilder $auth $name $version
+			fi
+		done
+
+
+		#build a map of all packages that are diretcly or indirectly required by
+		#the target package. This prevents removing directories not being used at all.
+
+		while read -r dep; do
+			stargetRequirementMapBuilder $dep
+		done < $libLoc$slash$1-$2-$3$slash$deps
+
+
+		#for all packages in the second map but not the first remove them.
+		for d in $( ls -d $libLoc$slash*/ ) ; do
+			#check deps file to see if it is an ospm module.
+			if [ ! -z "${targetRequiredPackages[$packageDir]}" ] && [ -z "${nonTargetRequiredPackages[$packageDir]}" ]; then
+				if [[ -f $d$slash$deps ]]; then
+					rm -rf $d
+				fi
+			fi
+		done
+
+		#remove target package.
+		if [[ -f $libLoc$slash$packageDir$slash$deps ]]; then
+			rm -rf $libLoc$slash$packageDir
+		fi
+
+	else
+		echo "Uninstall unsuccesful some error with library location or input."
+	fi
+}
+
+function uninstall {
+#only do one uninstall
+
+
+	if [ ! -z $libLoc ] && [ "$libLoc" != "/" ] && [ ! -z $1 ] && [ ! -z $2 ]; then
+		libLoc=$(cat /usr/local/lib/ospmLibSettings)
+		if [ "$1" == "rec" ]; then
+			#check if target is required by another package
+				#if it is stop
+				#else if it is not required by another package
+					#build map
+						#for each p of dir
+							#if not target
+								#add to map
+								#unisntall helper
+
+
+
+
+
+
+
+
+			beingUsed=false
+
+			#check to see if other packages require this one.
+			for d in $( ls -d $libLoc$slash*/ ) ; do #this wont work :(
+				# if [[ -L "${f%/}" ]]; then
+				echo "$d"
+				# fi
+				if grep -Fxq "$2 $3 $4" $d$slash$deps && "$d" != "$5-$6-$7"
+				then
+					echo "$2-$3-$4 is being used in $d"
+					beingUsed=true
+				fi
+			done
+		#if other packages do require this one then DONE.
+		if $beingUsed ; then
+			echo "$2-$3-$4 not uninstalled"
+			return
+			#if they do not then run full delete for each pacakge.
+		else
+
+			while read -r dep; do
+				source opsm.sh uninstall full $dep $1 $2 $3
+			done < $libLoc$slash$1-$2-$3$slash$deps
+
+			rm -rf $libLoc$slash$1-$2-$3
+		fi
+
+	elif [ "$1" == "safe" ]; then
+		beingUsed=false
+		for d $( ls -d $libLoc$slash*/ ) ; do #this wont work :(
+		# if [[ -L "${f%/}" ]]; then
+		echo "$d"
+		# fi
+		if grep -Fxq "$2 $3 $4" $d$slash$deps
+		then
+			echo "$2-$3-$4 is being used in $d"
+			beingUsed=true
+		fi
+	done
+	if $beingUsed ; then
+		echo "$2-$3-$4 not deleted"
+	else
+		$libLoc$slash$1-$2-$3
+		rm -rf $libLoc$slash$1-$2-$3
+	fi
+elif [ "$1" == "force" ]
+slash="/"
+rm -rf $libLoc$slash$1-$2-$3
+else
+	#todo some error message
+fi
+
+fi
+#todo some error message
+
+
+
 }
 
 function install {
 	libLoc=$(cat /usr/local/lib/ospmLibSettings)
 	if [[ ! -z $libLoc ]]; then
 		if [[ "$1" == "list" ]]; then
-			echo "list"
-			echo $2
 			if [ -f "$2" ]; then
 				while read -r dep; do
 					printf "${YELLOW}$dep\n${NC}"
 					dep_dir=$libLoc$slash$dep
 					if [ ! -z "$dep" ] && [ "$dep" != "\n" ]; then
-							source ospm.sh install $dep
+						source ospm.sh install $dep
 					fi
 				done <$2
 			fi
@@ -57,7 +243,7 @@ function install {
 						printf "${YELLOW}$dep\n${NC}"
 						dep_dir=$libLoc$slash$dep
 						if [ ! -z "$dep" ] && [ "$dep" != "\n" ]; then
-								source ospm.sh install $dep
+							source ospm.sh install $dep
 						fi
 					done <$saveLoc$slash$deps
 				fi
@@ -77,18 +263,21 @@ function install {
 
 case "$1" in
 	"library" )
-     		library $2 $3
-		;;
-		"install" )
-			install $2 $3 $4
-		;;
-    "-v" )
-                printf "${YELLOW}ospm 0.0.1\n${NC}"
-                ;;
+	library $2 $3
+	;;
+	"install" )
+	install $2 $3 $4
+	;;
+	"uninstall" )
+	uninstall $2 $3 $4
+	;;
+	"-v" )
+	printf "${YELLOW}ospm 0.0.1\n${NC}"
+	;;
 
-    "-h" )
-                help
-                ;;
+	"-h" )
+	help
+	;;
 	*)
-        	printf "${RED}command not found\n${NC}";
+	printf "${RED}command not found\n${NC}";
 esac
